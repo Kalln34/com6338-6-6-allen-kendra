7dc83396b6d74557bf9634b8ca1e83861b51006d
Kendra Allen <kallen5@ufl.edu>
2025-06-20 22:28:51 -0400

    modified index.js

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 var menu = document.getElementById('main-menu');
 var hamburgerBtn = document.querySelector('.hamburger-btn');
 
+
+// open menu
 function openMenu() {
     menu.classList.add('show-menu');
     hamburgerBtn.setAttribute ('aria-expanded', 'true');
 }
 
+// close menu
 function closeMenu() {
     menu.classList.remove('show-menu');
     hamburgerBtn.setAttribute('aria-expanded', 'false');
@@ -26,9 +29,19 @@ hamburgerBtn.addEventListener('click', function(event) {
 });
 
 document.addEventListener('click', function(event) {
-    var isClickInside = menu.contains(event.target) || hamburgerBtn.contains(event.target);
-    if (!isClickInside) {
+    var isInside = menu.contains(event.target) || hamburgerBtn.contains(event.target);
+    if (!isInside) {
         closeMenu();
     }
 });
+
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'Escape' || event.key === 'Esc') {
+        if (menu.classList.contains('show-menu')) {
+            closeMenu();
+            hamburgerBtn.focus();
+        }
+    }
+});
+
 
